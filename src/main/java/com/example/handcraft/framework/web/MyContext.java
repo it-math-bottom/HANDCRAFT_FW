b668a5677f1b870117626c4fc2d91beac3d5fde9
it-math-bottom <free.min1123t@gmail.com>
2022-11-20 18:59:55 +0900

    feat：MyComponentアノテーションが付与されたクラスをDIコンテナに登録する仕組みを構築

--- src/main/java/com/example/handcraft/framework/web/MyContext.java
+++ src/main/java/com/example/handcraft/framework/web/MyContext.java
@@ -1,17 +1,45 @@
 package com.example.handcraft.framework.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.example.handcraft.framework.web.annotation.MyAutowire;
+import com.example.handcraft.framework.web.annotation.MyComponent;
 
 public class MyContext {
 
 	private static final Map<String, Class<?>> TYPES_MAP = new HashMap<String, Class<?>>();
 	private static final Map<String, Object>   BEANS_MAP = new HashMap<String, Object>();
+	
+	private static final Path CLASS_PATH;
+	static {
+		final URL thisResource = MyContext.class.getResource(
+				"/" + MyContext.class.getName().replace('.', '/') + ".class");
+		try {
+			CLASS_PATH = new File(thisResource.toURI()).toPath()
+					.getParent()
+					.getParent()
+					.getParent()
+					.getParent()
+					.getParent()
+					.getParent();
+		} catch (URISyntaxException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 	
 	/**
 	 * コンテナへの登録を行う。
@@ -23,6 +51,32 @@ public class MyContext {
 	public static void registerType(final String name, final Class<?> type) {
 		TYPES_MAP.put(name, type);
 	}
+	
+	/**
+	 * クラスパス配下の{@link MyComponent}付与クラスを対象に、コンテナの登録を行う。
+	 * ただし、この時点で行うのは型の登録のみで、インスタンス生成はBeanの取得時に行う。
+	 */
+	public static void autoRegister() {
+		try (final Stream<Path> filesStream = Files.walk(CLASS_PATH)) {
+			filesStream
+				.filter(path -> !Files.isDirectory(path))                      // ファイルのみを対象とする
+				.filter(path -> path.toString().endsWith(".class"))            // .classのみを対象とする
+				.map(path -> CLASS_PATH.relativize(path))                       // クラスパスとの相対パスに変換する
+				.map(path -> path.toString().replace(File.separatorChar, '.')) // セパレータを '.' にする
+				.map(name -> StringUtils.substringBeforeLast(name, ".class"))  // 拡張子.classを除外する（FQCNになる）
+				.map(name -> {
+					try {
+						return Class.forName(name);
+					} catch (ClassNotFoundException  ex) {
+						throw new RuntimeException(ex);
+					}})                                                         // FQCNを元にクラス情報に変換する
+				.filter(clazz -> clazz.isAnnotationPresent(MyComponent.class))
+				.forEach(clazz -> registerType(clazz.getName(), clazz));
+		} catch (IOException ex) {
+			throw new UncheckedIOException(ex);
+		}
+	}		
+
 	
 	/**
 	 * Beanの取得を行う。
--- src/main/java/com/example/handcraft/framework/web/SampleMainProgram.java
+++ src/main/java/com/example/handcraft/framework/web/SampleMainProgram.java
@@ -1,15 +1,13 @@
 package com.example.handcraft.framework.web;
 
 import com.example.handcraft.framework.web.beans.BarBean;
-import com.example.handcraft.framework.web.beans.FooBean;
 
 public class SampleMainProgram {
 
 	public static void main(String[] args) {
 		
-		MyContext.registerType(FooBean.class.getName(), FooBean.class);
-		MyContext.registerType(BarBean.class.getName(), BarBean.class);
-		
+		MyContext.autoRegister();
+
 		BarBean bar = (BarBean) MyContext.getBean(BarBean.class.getName());
 		bar.showMyName();
 		
