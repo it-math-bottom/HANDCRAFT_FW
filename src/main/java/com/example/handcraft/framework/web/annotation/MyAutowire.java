b57383e3b999448366125bf45ebe41e457215817
it-math-bottom <free.min1123t@gmail.com>
2022-11-20 16:51:04 +0900

    fix：アノテーションをフィールドに付与できるようにするための対応 ※これをやらないと、フィールドにアノテーション付与しても有効なアノテーションだと認識してもらない （リフレクションで取得したフField情報の中のannotation属性がnullになっていた）

--- src/main/java/com/example/handcraft/framework/web/annotation/MyAutowire.java
+++ src/main/java/com/example/handcraft/framework/web/annotation/MyAutowire.java
@@ -1,4 +1,13 @@
 package com.example.handcraft.framework.web.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = { ElementType.FIELD })
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
 public @interface MyAutowire {
 }
